fe34f98b121e0985c4e10af52b1cdf5d9a741024
racheal1227 <89831637+racheal1227@users.noreply.github.com>
2021-12-01 09:30:38 +0900

    로그 수정(상세)
    Controller, ServiceImpl, Dao 메소드, 아규먼트, return value 로그로 확인 가능
    각각 시작 할 때 [Ctrl, Srv, DAO class: ..., method: ...]
    끝날 때 [Ctrl, Srv, DAO: (수행시간)ms]

--- src/main/java/com/mycompany/artistar/common/service/AroundLog.java
+++ src/main/java/com/mycompany/artistar/common/service/AroundLog.java
@@ -5,12 +5,14 @@ import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Service;
+import org.springframework.ui.Model;
 import org.springframework.util.StopWatch;
+import org.springframework.web.servlet.ModelAndView;
 
 @Service
 @Aspect
 public class AroundLog {
-	@Pointcut("execution(public * com.mycompany.artistar..*Service.*(..))")
+	@Pointcut("execution(public * com.mycompany.artistar..*Impl.*(..))")
 	public void commonServicePointcut() {
 	}
 	@Pointcut("execution(public * com.mycompany.artistar..*Controller.*(..))")
@@ -19,51 +21,67 @@ public class AroundLog {
 	@Pointcut("execution(public * com.mycompany.artistar..*Dao.*(..))")
 	public void commonDaoPointcut() {
 	}
+	
 	//TODO:나중에 필요한 걸로 바꾸기!
 	@Around("commonServicePointcut()")
 	public Object aroundServiceLog(ProceedingJoinPoint pjp) throws Throwable {
-		System.out.println("aroundLog 메소드!!!!!" + pjp.getThis() + "target name : " + pjp.getSignature().getName());
+		System.out.println("[Srv class:" + pjp.getThis() + ", method:" + pjp.getSignature().getName() + "]");
+		Object[] args = pjp.getArgs();
+		for(int i=0;i<args.length; i++) {
+			System.out.print("args["+i+"]: " + args[i] + ",\n");
+		}
 		
 		StopWatch sw = new StopWatch();
 		sw.start();
 		
 		Object ro = pjp.proceed();
 		sw.stop();
-		System.out.println("메소드 수행시간 : " + sw.getTotalTimeMillis() + "ms");
+		
 		if(ro!=null) {
-			System.out.println("target return : " + ro.toString());
+			System.out.println("return val: " + ro.toString());
 		}
+		System.out.println("[Srv: " + sw.getTotalTimeMillis() + "ms]");
 		return ro;
 	}
 	@Around("commonControllerPointcut()")
 	public Object aroundControllerLog(ProceedingJoinPoint pjp) throws Throwable {
-		System.out.println("aroundLog 메소드!!!!!" + pjp.getThis() + "target name : " + pjp.getSignature().getName());
+		System.out.println("[Ctrl class:" + pjp.getThis() + ", method:" + pjp.getSignature().getName() + "]");
+		Object[] args = pjp.getArgs();
+		for(int i=0;i<args.length; i++) {
+			if(!(args[i] instanceof ModelAndView))
+				System.out.print("args["+i+"]: " + args[i] + ",\n");
+		}
 		
 		StopWatch sw = new StopWatch();
 		sw.start();
 		
 		Object ro = pjp.proceed();
 		sw.stop();
-		System.out.println("메소드 수행시간 : " + sw.getTotalTimeMillis() + "ms");
+		
 		if(ro!=null) {
-			System.out.println("target return : " + ro.toString());
+			System.out.println("return val: " + ro.toString());
 		}
+		System.out.println("[Ctrl: " + sw.getTotalTimeMillis() + "ms]");
 		return ro;
 	}
 	@Around("commonDaoPointcut()")
 	public Object aroundDaoLog(ProceedingJoinPoint pjp) throws Throwable {
-		System.out.println("aroundLog 메소드!!! "+ pjp.getThis() + " target name: "+ pjp.getSignature().getName());
+		System.out.println("[DAO class:" + pjp.getThis() + ", method:" + pjp.getSignature().getName() + "]");
+		Object[] args = pjp.getArgs();
+		for(int i=0;i<args.length; i++) {
+			System.out.print("args["+i+"]: " + args[i] + ",\n");
+		}
 		
 		StopWatch sw = new StopWatch();
 		sw.start();
 		
-		Object ro = pjp.proceed();   // 타겟 메소드 실행 - 타겟 메소드에 throws가 걸려있을 수 있으므로 이메소드에서도 throws Throwable  작성해주어야함. try/catch 하지말기!
-		
+		Object ro = pjp.proceed();
 		sw.stop();
-		System.out.println("메소드 수행시간: "+ sw.getTotalTimeMillis() +"ms");
-		if(ro != null) {
-			System.out.println("target return: "+ ro.toString());
+		
+		if(ro!=null) {
+			System.out.println("return val: " + ro.toString());
 		}
+		System.out.println("[DAO: " + sw.getTotalTimeMillis() + "ms]");
 		return ro;
 	}
 }
