c3136977972cf012fcb707de9280ed2217c82d3c
KyoungSook1 <Kyoungsook001@gmail.com>
2021-12-10 11:24:41 +0900

    회원 탈퇴 css, 비밀번호 변경 controller 작성

--- src/main/java/com/mycompany/artistar/member/controller/MemberController.java
+++ src/main/java/com/mycompany/artistar/member/controller/MemberController.java
@@ -128,6 +128,27 @@ public class MemberController {
 		}
 	}
 
+	// 비밀번호 변경 get
+	@RequestMapping(value = "/pwChange", method = RequestMethod.GET)
+	public String pwChange() throws Exception {
+		return "member/pwChange2";
+	}
+
+	// 비밀번호 변경 post
+	@RequestMapping(value = "/pwChange", method = RequestMethod.POST)
+	public String pwChange(Member member, HttpSession session, RedirectAttributes rttr)  {
+		try {
+			memberService.pwChange(member);
+			session.setAttribute("member", member);
+			rttr.addFlashAttribute("message", "비밀번호 변경이 완료되었습니다.");
+			return "redirect:/pwChange";
+		} catch (Exception e) {
+			e.printStackTrace();
+			rttr.addFlashAttribute("message", "비밀번호 변경에 실패했습니다. 다시 시도해주세요.");
+			return "member/pwChange";
+		}
+	}
+
 	// 회원 탈퇴 get
 	@RequestMapping(value = "/memberdelete", method = RequestMethod.GET)
 	public String memberdelete() throws Exception {
